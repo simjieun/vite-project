b0b859853e952c2e4ea68083c38fbd5d9a90ad3d
simjieun <simjiuen@gmail.com>
2023-04-20 23:38:30 +0900

    tailwindcss 세팅 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import List from "./components/pages/List";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>,
-        },
-    ])
-    return (
-        <RouterProvider router={router}/>
-    );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <List />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/pages/List/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const List: FC = () => {
+  return (
+    <div className="grid-rows-[auto 1fr] grid h-full">
+      <div className="h-54 mb-0">
+        <header className="bg-violet">HTTP 203</header>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+export default List;
